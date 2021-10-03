ae1fec94c265e31712b3682fd00d6e9c95987fd9
Shivansh Gupta <shiv0403gupta@gmail.com>
2021-10-04 00:36:00 +0530

    replaced chimps from euphoria

--- src/WhyChimps/data.js
+++ src/WhyChimps/data.js
@@ -1,37 +1,30 @@
 export const data = [
   {
     title: "EXPERIENCE",
-    desc:
-      "Hands-on experience across domains and technologies gives a cutting edge to Chimps.",
+    desc: "Hands-on experience across domains and technologies gives a cutting edge to Euphoria.",
   },
   {
     title: "TIME-TESTED",
-    desc:
-      " See your ideas take shape in real-time-facilitating on-the-board iterations.",
+    desc: " See your ideas take shape in real-time-facilitating on-the-board iterations.",
   },
   {
     title: "CUSTOMIZATION",
-    desc:
-      "Horses for Courses is our funda. Customized solutions are what we are excellent at.",
+    desc: "Horses for Courses is our funda. Customized solutions are what we are excellent at.",
   },
   {
     title: "VALUE ADDITION",
-    desc:
-      "Our value-added business services and solutions add a spring to your portfolio.",
+    desc: "Our value-added business services and solutions add a spring to your portfolio.",
   },
   {
     title: "TIMELY DELIVERIES",
-    desc:
-      "A one-stop technology solutions centre where Time is honored – We deliver in time- everytime.",
+    desc: "A one-stop technology solutions centre where Time is honored – We deliver in time- everytime.",
   },
   {
     title: "SECURED AND ECONOMICAL",
-    desc:
-      "Being at the top, our technology solutions are highly secured to ensure confidentiality and economy to clients.",
+    desc: "Being at the top, our technology solutions are highly secured to ensure confidentiality and economy to clients.",
   },
   {
     title: "QUALITY SERVICES",
-    desc:
-      "Stringent quality checks to weed out bugs, operational road blocks and smoothen customer journey.",
+    desc: "Stringent quality checks to weed out bugs, operational road blocks and smoothen customer journey.",
   },
 ];
